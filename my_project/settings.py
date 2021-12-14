2b2f6e895f82e5f7724d28c9548f35a21ec4251d
forza111 <nikitos.ionkin@yandex.ru>
2021-12-14 09:00:53 +0300

    change login.html and logout html

--- my_project/settings.py
+++ my_project/settings.py
@@ -127,7 +127,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/bookings/parkings'
+LOGOUT_REDIRECT_URL = '/bookings/parkings'
 
 
 # Static files (CSS, JavaScript, Images)
